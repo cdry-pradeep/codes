8bfb6682cbcbf4ada19e6ba3def921f0a399101e
cdry-pradeep <106676295+cdry-pradeep@users.noreply.github.com>
2025-02-11 16:24:45 +0545

    Add files via upload

--- replace 1st occerence of vowel.c
+++ replace 1st occerence of vowel.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+// Function to replace the first occurrence of a vowel with '-'
 void replaceFirstVowel(char str[]) {
+    //String that contains lowercase and uppercase vowels
+    //We will use this to check the vowels in input string
     char vowels[] = "aeiouAEIOU";
     int i, j;
 
+    // Traverse the string to find the first vowel
     for (i = 0; str[i] != '\0'; i++) {
         for (j = 0; vowels[j] != '\0'; j++) {
             if (str[i] == vowels[j]) {
@@ -16,8 +20,14 @@ void replaceFirstVowel(char str[]) {
 
 int main() {
     char str[100]; // Array to hold input string
+
+    // Prompt the user to enter a string
     printf("Enter a string: ");
+
+    // Read the input string and store in array str[]
     fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
     int len = 0;
     while (str[len] != '\0') {
         if (str[len] == '\n') {
@@ -27,7 +37,10 @@ int main() {
         len++;
     }
 
+    // Call the function to replace the first vowel
     replaceFirstVowel(str);
+
+    // Print the modified string
     printf("Modified string: %s\n", str);
 
     return 0;
